f75dc32654a849f5135407f7e6a63f7af2397329
dutraneto <dutraneto@me.com>
2021-02-24 15:02:13 -0500

    Implement vanilla smooth scroll to page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('loaded');
+// Smooth scroll control
+// https://dev.to/surjithctly/solved-2020-pure-vanilla-javascript-smooth-scroll-to-element-on-a-click-id-2cek
+let triggers = document.querySelectorAll('.logo[href="#hero"],.nav__item .nav__link[href^="#"]')
+console.log(triggers)
+
+triggers.forEach((trigger) => {
+    trigger.onclick = function (e) {
+        e.preventDefault()
+        let hashTag = this.getAttribute('href')
+        let target = document.querySelector(hashTag)
+        let headerOffset = 0
+        let elementPosition = target.offsetTop
+        let offsetPosition = elementPosition - headerOffset
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+        })
+    }
+})
